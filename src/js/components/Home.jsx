777af012b6bbded32e976be977d067d31d664dc6
Jerimar-DV <69823365+Jerimar-DV@users.noreply.github.com>
2026-04-12 15:08:33 +0000

    Add GET metho

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const API = "https://playground.4geeks.com/todo/todos/alesanchezr"
 
 //create your first component
 const Home = () => {
+
+	const GetProducts = () => {
+		fetch('${API/USER}', {
+			method: "GET",
+			body: JSON.stringify(task),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				if(!resp.ok) throw Error('Ha ocuerrido algun error')
+				return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+			})
+			.then(respJson => {
+				// Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				console.log(respJson); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+			})
+			.catch(error => {
+				// Manejo de errores
+				console.log(error);
+			});
+	}
+
+	//let nuevoTodo="";
+	const [nuevoTodo, setNuevoTodo] = useState("Tarea nueva");
+	const [todos, setTodos] = useState(["Una tarea", "Dos tarea", "tres tareas"])
+
+	const handleClick = () => {
+		console.log("Nueva tarea", nuevoTodo);
+		setTodos([...todos, nuevoTodo])
+	}
+
+	const deleteTodo = (indice) => {
+		console.log(indice);
+		const listNueva = todos.filter((todo, i) => i !== indice)
+		setTodos(listNueva);
+	}
+
+	const handleChange = (event) => {
+		setNuevoTodo(event.target.value);
+
+		//console.log(event.target.value);
+		//setNuevoTodo (event.target.value);
+		//console.log("Saludo desde el evento de cambio")
+	}
+
 	return (
 		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">Shopping List</h1>
+			<div>
+				<input type="text" onChange={handleChange} />
+				<button onClick={handleClick}>Add task</button>
+			</div>
+			<p>New Task: {nuevoTodo}</p>
+			<ul>
+				{todos.map((todos, indice) => {
+					return (
+						<li>
+							{todos} <button onClick={() => deleteTodo(indice)}>Borrar</button>
+						</li>
+					)
+				})}
+			</ul>
 		</div>
 	);
 };
